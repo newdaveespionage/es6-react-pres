e3d2013c4a1098726764272b0a7eea37be6af4bf
DCox-Espenlaub <DCox-Espenlaub@dcox-espenlaub.local>
2015-07-31 16:06:42 -0700

    added search display and clear

--- empty/jsx/AppHeader.jsx
+++ empty/jsx/AppHeader.jsx
@@ -1,9 +1,43 @@
 const React = require('react');
 class AppHeader extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			term: ""
+		};
+	}
 	handleLayoutEvent(event) {
 		this.props.changeLayout(event.target.value);
 	}
+	handleTermEvent(event){
+		this.setState({term: event.target.value});
+	}
+	handleTermSubmit(event){
+		event.preventDefault();
+
+		this.props.search(this.state.term);
+		this.setState({term: ""});
+	}
+	handleClearEvent(e) {
+    	this.props.clearTerm();
+	}
 	render(){
+		let searchBox;
+		if(this.props.queryTerm){
+			searchBox = (
+				<h3 className="app-header__term">
+					{this.props.queryTerm}
+					<a href="#" onClick={this.handleClearEvent.bind(this)}><i className="fa fa-times" /></a>
+				</h3>
+			);
+		}else{
+			searchBox = (
+				<form onSubmit={this.handleTermSubmit.bind(this)}>
+					<input onChange={this.handleTermEvent.bind(this)} value={this.state.term} className="app-header__search" type="text" placeholder="Search" />
+				</form>
+			);
+		}
 		return(
 			<header className="app-header">
 				<div className="app-header__inner">
@@ -12,6 +46,7 @@ class AppHeader extends React.Component{
 						<option value="tile">Tile</option>
 						<option value="list">List</option>
 					</select>
+					{searchBox}
 				</div>
 			</header>
 		)
--- empty/jsx/index.jsx
+++ empty/jsx/index.jsx
@@ -2,7 +2,9 @@ const React = require('react');
 const preload = require('./netflix');
 const MovieContainer = require('./MovieContainer');
 const MovieTileLayout = require('./MovieTileLayout');
+const MovieListLayout = require('./MovieListLayout');
 const AppHeader = require('./AppHeader');
+const omdb = require('omdb-client');
 const _ = require('lodash');
 
 class App extends React.Component {
@@ -11,19 +13,42 @@ class App extends React.Component {
 
 		this.state = {
 			results: _.clone(preload.Search),
-			layout: 'tile'
+			layout: 'tile',
+			queryTerm: ''
 		};
+	}
+	search(term){
+		this.setState({queryTerm:term});
+		omdb.search({query:term}, (err,data) => {
+			this.setState({results: data.Search});
+		});
 	}
 	changeLayout(layout){
 		this.setState({layout});
 	}
+	clearTerm(){
+		this.setState({
+			queryTerm:"",
+			results: _.clone(preload.Search, true)
+		});
+	}
 	render(){
 		// unique key allows react to perform better with dom updates
+		let layout;
+
+		if(this.state.layout === 'tile'){
+			layout = MovieTileLayout;
+		}else{
+			layout = MovieListLayout;
+		}
 		return (
 			<div className="app-container">
 				<AppHeader 
 					layout={this.state.layout}
 					changeLayout={this.changeLayout.bind(this)}
+					search={this.search.bind(this)}
+					queryTerm={this.state.queryTerm}
+					clearTerm={this.clearTerm.bind(this)}
 				/>
 				<div className="movies-list">
 					{this.state.results.map( el => {
@@ -31,7 +56,7 @@ class App extends React.Component {
 							<MovieContainer
 								id={el.imdbID}
 								key={el.imdbID}
-								layout={MovieTileLayout}
+								layout={layout}
 							/>
 						);
 					})}
